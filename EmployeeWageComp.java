5eb1db58ea5d51a0152bd8528aa363a480074d5c
RajendraGund <rajendragund2015@gmail.com>
2021-08-24 19:48:44 +0530

    Welcome sms

---
+++ EmployeeWageComp.java
@@ -0,0 +1,7 @@
+package employee.wage;
+
+public class EmployeeWageComp {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation program !! ");
+	}
+}
